c99c290964341d78880b45b858b56eb9ab5bba9c
Jonas Uekoetter <github@lazycoder.de>
2016-09-27 18:43:24 +0200

    Android: fix binding in BaseActivity for SupportActionBar

--- CommitStrip/CommitStrip.Droid/Activities/BaseActivity.cs
+++ CommitStrip/CommitStrip.Droid/Activities/BaseActivity.cs
@@ -1,15 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Android.App;
-using Android.Content;
 using Android.OS;
 using CommitStrip.Core.ViewModels;
 using MvvmCross.Binding.BindingContext;
 using MvvmCross.Droid.Support.V7.AppCompat;
-using MvvmCross.Droid.Views;
 
 
 namespace CommitStrip.Droid.Activities
@@ -22,7 +17,7 @@ namespace CommitStrip.Droid.Activities
             base.OnCreate(savedInstanceState);
 
             var set = this.CreateBindingSet<BaseActivity<T>, T>();
-            set.Bind(ActionBar).For(v => v.Title).To(vm => vm.Title);
+            set.Bind(SupportActionBar).For(v => v.Title).To(vm => vm.Title);
             set.Apply();
         }
     }
