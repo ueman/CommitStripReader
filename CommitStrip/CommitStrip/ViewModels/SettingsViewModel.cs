239833aaa0644c5ec54e5fd2197315a4a12a6407
Jonas Uekoetter <github@lazycoder.de>
2016-09-28 21:38:55 +0200

    Core: add SettingsViewModel

---
+++ CommitStrip/CommitStrip/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommitStrip.Core.Services;
+using MvvmCross.Core.ViewModels;
+
+namespace CommitStrip.Core.ViewModels
+{
+    public class SettingsViewModel : BaseViewModel
+    {
+        public string Title { get; set; }
+
+        public SettingsViewModel(INetworkConnectivityService connectivityService) : base(connectivityService)
+        {
+            Title = "Settings";
+        }
+
+        #region Commands
+
+        private MvxCommand _openCsCommand;
+        public IMvxCommand OpenCsPageCommand
+        {
+            get
+            {
+                _openCsCommand = _openCsCommand ?? new MvxCommand(OpenCsWebPage);
+                return _openCsCommand;
+            }
+        }
+
+        private void OpenCsWebPage()
+        {
+            //show commit strip page
+        }
+
+        #endregion
+    }
+}
