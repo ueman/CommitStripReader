64c3b4d60f0d77ebab0f6eba998e0c10aded7d87
Jonas Uekoetter <github@lazycoder.de>
2016-10-01 22:53:20 +0200

    cleanup constants

--- CommitStrip/CommitStrip/Common/Constants.cs
+++ CommitStrip/CommitStrip/Common/Constants.cs
@@ -2,24 +2,11 @@
 {
     public class Constants
     {
-        public static string ComicFeed
-        {
-            get { return "http://www.commitstrip.com/en/feed"; }
-        }
+        public static string ComicFeed => "http://www.commitstrip.com/en/feed";
 
         public static string ComicFeedPage(int page)
         {
             return ComicFeed + "?paged=" + page;
-        }
-
-        public static string ComicFeedJson
-        {
-            get { return "http://rss2json.com/api.json?rss_url=http://www.commitstrip.com/en/feed"; }
-        }
-
-        public static string ComicFeedPageJson(int page)
-        {
-            return ComicFeedJson + "?paged=" + page;
         }
 
         public static string CommitStripLink = "http://commitstrip.com/";
