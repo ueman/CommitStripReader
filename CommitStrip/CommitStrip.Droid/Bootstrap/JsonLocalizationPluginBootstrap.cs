be77f1e3cc34c0cbd778519055ea54ab176569db
Jonas Uekoetter <github@lazycoder.de>
2016-10-01 18:24:26 +0200

    added json localization plugin

---
+++ CommitStrip/CommitStrip.Droid/Bootstrap/JsonLocalizationPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace CommitStrip.Droid.Bootstrap
+{
+    public class JsonLocalizationPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.JsonLocalization.PluginLoader>
+    {
+    }
+}
---
+++ CommitStrip/CommitStrip.Droid/Bootstrap/ResourceLoaderPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace CommitStrip.Droid.Bootstrap
+{
+    public class ResourceLoaderPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.ResourceLoader.PluginLoader>
+    {
+    }
+}
