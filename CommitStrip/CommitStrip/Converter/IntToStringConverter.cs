55ed6fff179c2d539c14b29c5dcbf36e241930bc
Jonas Uekoetter <github@lazycoder.de>
2016-10-01 18:25:06 +0200

    added int to string converter

---
+++ CommitStrip/CommitStrip/Converter/IntToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using MvvmCross.Platform.Converters;
+
+namespace CommitStrip.Core.Converter
+{
+    public class IntToStringConverter : MvxValueConverter<int, string>
+    {
+        protected override string Convert(int value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        protected override int ConvertBack(string value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return 0;
+            }
+            return int.Parse(value);
+        }
+    }
+}
